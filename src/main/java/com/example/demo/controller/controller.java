62341c1c3c5a8aaf228bdb1d8f2c80424ee02434
Kishor1212 <cyberion.empire7@gmail.com>
2023-02-24 09:05:58 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/controller.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	
+	@Value("${value:kishor}")
+	private String name;
+	
+	@GetMapping("/")
+	public String display() {
+		return "Hey... "+name;
+	}
+}
---
+++ src/test/java/com/example/demo/TestApp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
